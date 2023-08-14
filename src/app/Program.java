18adc7ebd739ea1b968ef84593894996bd4c0fe5
Vinicius Gabriel <vinicius.gabriel83@yahoo.com>
2023-08-14 17:11:28 -0300

    Seller entity class

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,11 +1,15 @@
 package app;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.sql.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(1985, 05, 02), 3000.0, obj);
+        System.out.println(seller);
     }
 }
